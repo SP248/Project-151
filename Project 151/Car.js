cf91b555e5f7e1a0ef630a7c06e54c4e9a31155b
SP248 <82733585+SP248@users.noreply.github.com>
2023-01-30 21:37:27 -0500

    Add files via upload

--- Project 151/Car.js
+++ Project 151/Car.js
@@ -1,11 +1,11 @@
 AFRAME.registerComponent("car-model", {
     schema: {
-      modelRef: { type: "string", default: "../assets/car/scene.gltf" }
+      modelRef: { type: "string", default: "Ferrari.glb" }
     },
     init: function() {
       this.el.setAttribute("gltf-model", this.data.modelRef);
-      const position = { x: 0, y: 50, z: 80 };
-      const rotation = { x: 0, y: -100, z: 0 };
+      const position = { x: 0, y: 10, z: 80 };
+      const rotation = { x: 0, y: 50, z: 0 };
       this.el.setAttribute("position", position);
       this.el.setAttribute("rotation", rotation);
     }
